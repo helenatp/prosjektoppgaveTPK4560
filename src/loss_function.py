db39b77aea9e0b7553cdef29ba306c94fa51501a
Vilde Taklo Kenworthy <vildetk@stud.ntnu.no>
2022-09-30 12:10:08 +0200

    Changed loss function

--- src/loss_function.py
+++ src/loss_function.py
@@ -18,11 +18,7 @@ n = 200
 x = randn(n)/2
 # y = 1 + 5*np.sin(x/10) + 5*x**2 + randn(n)
 y=np.sin(x) + randn(n)
-x_min = np.min(x)
-x_max = np.max(x)
-y_min = np.min(y)
-y_max = np.max(y)
-lam = 0.0001
+lam = 0.000001
 
 '''
 Different kernels
@@ -44,11 +40,9 @@ def zs(x):
 def K_S(x, y):
     return np.sum(zc(x) * zc(y)) + np.sum(zs(x) * zs(y))
 
-# Exact Gaussian kernel
+# Gaussian kernel
 def K_G(x, y): 
     return np.exp(-(x-y)**2/sigma2)
-
-#print(K_G(2, 3))
 
 # Find the constant a 
 def K(k, x, y):
@@ -83,7 +77,7 @@ Ky_S = K(K_S, x, y)
 Kx_S = K(K_S, x, x)
 normf_S = norm_f2(Ky_S, a_S)
 
-# With exact kernel
+# With Gaussian kernel
 a_G = a(K_G, x, y)
 Ky_G = K(K_G, x, y)
 Kx_G = K(K_G, x, x)
@@ -91,25 +85,29 @@ normf_G = norm_f2(Ky_G, a_G)
 
 x_sort = np.sort(x)
 
-def loss_function(Kx, a, x, y, normf):
+def loss_function(k, a, x, y, normf):
     n = len(x)
     sum = 0
     for i in range(n):
-        sum += (a[i]*Kx[i] - y[i])**2
+        f_i = 0
+        for j in range(n):
+            f_i += a[j]*k(x[i], x[j])
+        sum += (f_i - y[i])**2
     return 1/len(x) * sum + lam *normf
 
-loss_S = loss_function(Kx_S, a_S, x, y, normf_S)
+loss_S = loss_function(K_S, a_S, x, y, normf_S)
 
-loss_G = loss_function(Kx_G, a_G, x, y, normf_G)
+loss_G = loss_function(K_G, a_G, x, y, normf_G)
 
-loss_RR = loss_function(Kx_RR, a_RR, x, y, normf_RR)
+loss_RR = loss_function(K_RR, a_RR, x, y, normf_RR)
+# ikke samme alpha på cos og sin i rahimi
 print("Loss Suzuki: ", loss_S)
 print("Loss Gaussaian: ", loss_G)
 
 plt.scatter(x,y, facecolors ='none' , edgecolors = "k" , marker = "o")
-plt.plot( x_sort, loss_G, c = "r" , label = "w/o Approx")
+"""plt.plot( x_sort, loss_G, c = "r" , label = "w/o Approx")
 plt.plot( x_sort, loss_S, c = "b" , label = "with Approx")
-plt.plot(x_sort, loss_RR, c = "y", label ="With Rahimi")
+plt.plot(x_sort, loss_RR, c = "y", label ="With Rahimi")"""
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Kernel Regression")

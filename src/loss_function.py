ab3f76e6b4aa8fbbd44b1284d8fc7f9d97dabd89
unknown <helenatp@stud.ntnu.no>
2022-10-27 11:08:09 +0200

    1st try

--- src/loss_function.py
+++ src/loss_function.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use("seaborn-ticks")
 from sklearn.decomposition import PCA
-from numpy.random import randn # Gaussian random numbe
+from numpy.random import randn # Gaussian random number
 #Define the constants and functions here
 m = 20
 sigma = 2
